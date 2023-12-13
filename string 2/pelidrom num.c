c974a3fb347a2e5ce9de7a6ceb8e389eca194f87
krupa parmar <149374671+Krupaparmar30@users.noreply.github.com>
2023-12-13 17:35:21 +0530

    Add files via upload

---
+++ string 2/pelidrom num.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+main()
+{
+//	 char name[20]="nitin";
+//	 
+
+
+    char n;
+    printf("enter the size string:");
+    scanf("%c",&n);
+    
+    char name[n];
+    printf("enter the string :");
+    scanf("%s",name);
+	 int i,j=4,cheak=0;
+	 for(i=0;i<5;i++)
+	 {
+	      if(name[i]!=name[j])
+	 	{
+	 		cheak=1;
+		 }
+		 j--;
+	 }
+	 if(cheak==1)
+	 {
+	 	printf("this is not pelidrom");
+	 }
+	 else
+	 {
+	 		printf("this is  pelidrom");
+	 }
+}
