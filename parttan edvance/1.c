e4c2fb81456f33fce80d11047da38dd0daf893bb
krupa parmar <149374671+Krupaparmar30@users.noreply.github.com>
2024-01-08 15:42:16 +0530

    Add files via upload

---
+++ parttan edvance/1.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+main()
+{
+	int i,j;
+	int c=1,z=1;
+	
+	int k=1;
+	
+	for(i=1;i<=10;i++)
+	{
+		for(c=1;c<i;c++)
+		{
+			printf(" - ");
+		}
+		for(j=i;j<=10;j++)
+		{
+			
+			printf(" %d",k);
+			if(k>=1&&k<=9)
+			{
+				printf(" ");
+			}
+			k=k+j;
+		}
+		for(j=9;j>=i;j--)
+		{
+			printf(" %d",k);
+			k=k+j;
+		}
+		z=z+i;
+		k=i+z;
+		printf("\n");
+	}
+}
